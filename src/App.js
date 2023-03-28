fa47ba3572da072288870433c1a42e9a2b0a314d
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-03-28 17:26:21 +0200

    portfolio

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import hamburger from "./Assets/img/icon-hamburger.svg";
 import closeburger from "./Assets/img/icon-close-menu.svg";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import About from "./components/About";
 import Language from "./components/Language";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
 import Footer from "./components/Footer";
 import Education from "./components/Education";
+import repo from "./Assets/img/repo.png";
+import todo from "./Assets/img/todo.png";
+import phonie from "./Assets/img/phonie.png";
 //import Loader from "./components/Loader/Loader"
 function App() {
   const [loading, setLoading] = useState(false);
@@ -27,23 +30,23 @@ function App() {
   return (
     <div className="App">
       <HelmetProvider>
-      <Helmet>
-        <title>Oyindamola</title>‍
-        <meta name="description" content="Oyindamola's Portfolio" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@user" />
-        <meta name="twitter:creator" content="@user" />
-        <meta name="twitter:title" content="Oyindamola" />
-        <meta name="twitter:description" content="My portfolio" />
-        <meta name="twitter:image" content="url_to_image" />
-        <meta property="og:title" content="Oyindamola" />
-        <meta property="og:description" content="My portfolio" />
-        <meta property="og:image" content="url_to_image" />
-        <meta property="og:site_name" content="Oyindamola" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="article" />
-        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
-      </Helmet>
+        <Helmet>
+          <title>Oyindamola</title>‍
+          <meta name="description" content="Oyindamola's Portfolio" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@user" />
+          <meta name="twitter:creator" content="@user" />
+          <meta name="twitter:title" content="Oyindamola" />
+          <meta name="twitter:description" content="My portfolio" />
+          <meta name="twitter:image" content="url_to_image" />
+          <meta property="og:title" content="Oyindamola" />
+          <meta property="og:description" content="My portfolio" />
+          <meta property="og:image" content="url_to_image" />
+          <meta property="og:site_name" content="Oyindamola" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="article" />
+          <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+        </Helmet>
       </HelmetProvider>
       <div className="loading">
         {loading ? (
@@ -83,29 +86,110 @@ function App() {
                   alt="close menu"
                   id="close-icon"
                 />
-                </NavLink>
-                <ul id="menu" className={`menu${navbarOpen ? "show" : ""}`}>
-                  <li>
-                    <NavLink href="/" style={{ textDecoration: "none", color: "white" }}>About</NavLink>
-                  </li>
-                  <li>
-                    <NavLink href="/Projects" style={{ textDecoration: "none", color: "white" }}>Projects</NavLink>
-                  </li>
-                  <li>
-                  <NavLink href="/" style={{ textDecoration: "none", color: "white" }}>Resume</NavLink>
-                  </li>
-                  <li>
-                    <NavLink href="/Footer" style={{ textDecoration: "none", color: "white" }}>Contact</NavLink>
-                      
-                  </li>
-                </ul>
+              </NavLink>
+              <ul id="menu" className={`menu${navbarOpen ? "show" : ""}`}>
+                <li>
+                  <NavLink
+                    href="/"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    href="/Projects"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    Projects
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    href="/"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    Resume
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    href="/Footer"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
             </div>
             <div className="main-content">
               <div className="container">
                 <About />
                 <Language />
-                <Projects />
-                 <Blogs />
+                <>
+                  <div className="project-container">
+                    <div className="row mb-2">
+                      {/* slideShow */}
+
+                      <div className="single-project col-md-4">
+                        <div className="single-content">
+                          <h3>Github Api </h3>
+                          <img
+                            src={repo}
+                            alt=""
+                            style={{ height: "180px", width: "100%" }}
+                          />
+                          <p className="p-3">
+                            This is project list individual repositories in the
+                            github, it also gives details about the repos.
+                          </p>
+                          <button>View Demo</button>
+                        </div>
+                      </div>
+                      <div className="single-project col-md-4">
+                        <div className="single-content">
+                          <h3>Todo App</h3>
+                          <img
+                            src={todo}
+                            alt=""
+                            style={{ height: "180px", width: "100%" }}
+                          />
+                          <p className="p-3">
+                            The app adds, deletes, modify todos to give a
+                            stress- free life making daily life organized
+                          </p>
+                          <button>View Demo</button>
+                        </div>
+                      </div>
+                      <div className="single-project col-md-4">
+                        <div className="single-content">
+                          <h3>Phonie Project</h3>
+                          <img
+                            src={phonie}
+                            alt=""
+                            style={{ height: "180px", width: "100%" }}
+                          />
+                          <p className="p-3">
+                            This project output the service provider logo
+                            whenever a nigeria is inputted into the input type
+                            box
+                          </p>
+                          <button>View Demo</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="arrow">
+                      <ul>
+                        <li>
+                          <NavLink>
+                            <Link href="/Projects" style={{textDecoration: "none", color: "white", fontFamily: "fantasy", display: "flex", justifyContent: "flex-end"}}>View More</Link>
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </>
+                <Blogs />
                 <Education />
                 <Footer />
               </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,11 @@ import navigation from "../Assets/img/navigation.png";
 import phonie from "../Assets/img/phonie.png";
 import quiz from "../Assets/img/quiz.png";
 import redactr from "../Assets/img/redactr.png";
+// import { NavLink } from "react-bootstrap";
 import repo from "../Assets/img/repo.png";
 import todo from "../Assets/img/todo.png";
+// import {motion} from 'framer-motion';
+// import {useRef, useEffect, useState} from "react"
 
 const Projects = () => {
   return (
@@ -25,32 +28,155 @@ const Projects = () => {
           Projects
         </h3>
       </div>
+
       <div className="project-container">
         <div className="row mb-2">
-          <div className="single-project col-md-4">
-            <div className="single-content">
-            <h3>Github Api </h3>
-            <img src={repo} alt="" style={{height: "180px", width: "100%"}}/>
-            <p className="p-3">This is project list individual repositories in the github, it also gives details about the repos.</p>
-            <button>View Demo</button>
-            </div>
-          </div>
-          <div className="single-project col-md-4">
-          <div className="single-content">
-            <h3>Todo App</h3>
-            <img src={todo} alt="" style={{height: "180px", width: "100%"}}/>
-            <p className="p-3">The app adds, deletes, modify todos to give a stress- free life making daily life organized</p>
-            <button>View Demo</button>
-            </div>
-          </div>
-          <div className="single-project col-md-4">
-          <div className="single-content">
-            <h3>Phonie Project</h3>
-            <img src={phonie} alt="" style={{height: "180px", width: "100%"}}/>
-            <p className="p-3">This project output the service provider logo whenever a nigeria is inputted into the input type box</p>
-            <button>View Demo</button>
-            </div>
-          </div>
+        {/* slideShow */}
+
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Github Api </h3>
+                <img
+                  src={repo}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This is project list individual repositories in the github, it
+                  also gives details about the repos.
+                </p>
+                <button>View Demo</button>
+              </div>
+            </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Todo App</h3>
+                <img
+                  src={todo}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  The app adds, deletes, modify todos to give a stress- free
+                  life making daily life organized
+                </p>
+                <button>View Demo</button>
+              </div>
+            </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Phonie Project</h3>
+                <img
+                  src={phonie}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This project output the service provider logo whenever a
+                  nigeria is inputted into the input type box
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Redactr Project</h3>
+                <img
+                  src={redactr}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This project output the service provider logo whenever a
+                  nigeria is inputted into the input type box
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Quiz Project</h3>
+                <img
+                  src={quiz}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  The project is a testing platform outputing random questions, evaluates and give result after done with the assessment.
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Navigation</h3>
+                <img
+                  src={navigation}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This is a page showing a sliding navigation.
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+<div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Mechanic Website</h3>
+                <img
+                  src={mechanic}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                 A landing page for a mecahanic website.
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div>
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Rating site</h3>
+                <img
+                  src={interactive}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This is a small page displaying an intrative rating, it can be used in e-commerce website or any webpage that requires rating feedback.
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Crowdfunding website</h3>
+                <img
+                  src={crowdfunding}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  This is a landing page of a crowdfunding website
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div> 
+ <div className="single-project col-md-4">
+              <div className="single-content">
+                <h3>Random Jokes</h3>
+                <img
+                  src={jokes}
+                  alt=""
+                  style={{ height: "180px", width: "100%" }}
+                />
+                <p className="p-3">
+                  The site is a Api fetch data to output random jokes.
+                </p>
+                <button>View Demo</button>
+                </div>
+                </div>
         </div>
       </div>
     </div>
